9850c3bc3643907cfec85205aed16b1d01f450b4
StevenWijnja <s.n.wijnja@student.utwente.nl>
2021-10-25 20:48:13 +0200

    shared memory

--- main.cu
+++ main.cu
@@ -1,194 +1,207 @@
+// This program computes matrix multiplication using shared memory tiling
+// By: Nick from CoffeeBeforeArch
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
 #include <iostream>
+#include <vector>
+
 #include <cuda_runtime.h>
 #include <chrono>
 #include <string>
 
-#define N 16
-//#define TRANSPOSED
-
 typedef std::chrono::high_resolution_clock Clock;
 
-int32_t matrixCGlobal[N][N];
+using std::cout;
+using std::generate;
+using std::vector;
+
+// Matrix dimensions
+constexpr int M = 13;
+constexpr int N = 13;
+constexpr int K = 13;
 
 int32_t matrixAGlobal[N * N] = {
-        14, 39, 117, 89, 111, 73, 79, 102, 52, 81, 123, 70, 39, 0, 0, 0,
-        82, 29, 125, 85, 51, 60, 102, 39, 120, 106, 19, 15, 58, 0, 0, 0,
-        124, 31, 32, 23, 19, 69, 60, 61, 10, 33, 72, 1, 91, 0, 0, 0,
-        96, 112, 32, 111, 90, 12, 63, 77, 47, 105, 115, 38, 90, 0, 0, 0,
-        13, 35, 23, 78, 57, 109, 122, 89, 21, 116, 86, 123, 113, 0, 0, 0,
-        27, 14, 80, 69, 9, 23, 106, 26, 115, 31, 6, 73, 112, 0, 0, 0,
-        53, 70, 64, 118, 121, 17, 6, 113, 30, 8, 5, 116, 66, 0, 0, 0,
-        12, 113, 71, 94, 98, 116, 2, 95, 66, 107, 54, 11, 34, 0, 0, 0,
-        90, 36, 81, 124, 73, 41, 105, 14, 127, 109, 87, 29, 2, 0, 0, 0,
-        84, 77, 56, 81, 21, 81, 110, 110, 123, 104, 113, 39, 54, 0, 0, 0,
-        75, 102, 44, 79, 61, 55, 90, 125, 52, 45, 4, 120, 12, 0, 0, 0,
-        20, 20, 105, 41, 20, 44, 108, 74, 72, 62, 76, 34, 111, 0, 0, 0,
-        38, 97, 124, 5, 97, 87, 85, 106, 12, 31, 87, 6, 77, 0, 0, 0
+        14, 39, 117, 89, 111, 73, 79, 102, 52, 81, 123, 70, 39,
+        82, 29, 125, 85, 51, 60, 102, 39, 120, 106, 19, 15, 58,
+        124, 31, 32, 23, 19, 69, 60, 61, 10, 33, 72, 1, 91,
+        96, 112, 32, 111, 90, 12, 63, 77, 47, 105, 115, 38, 90,
+        13, 35, 23, 78, 57, 109, 122, 89, 21, 116, 86, 123, 113,
+        27, 14, 80, 69, 9, 23, 106, 26, 115, 31, 6, 73, 112,
+        53, 70, 64, 118, 121, 17, 6, 113, 30, 8, 5, 116, 66,
+        12, 113, 71, 94, 98, 116, 2, 95, 66, 107, 54, 11, 34,
+        90, 36, 81, 124, 73, 41, 105, 14, 127, 109, 87, 29, 2,
+        84, 77, 56, 81, 21, 81, 110, 110, 123, 104, 113, 39, 54,
+        75, 102, 44, 79, 61, 55, 90, 125, 52, 45, 4, 120, 12,
+        20, 20, 105, 41, 20, 44, 108, 74, 72, 62, 76, 34, 111,
+        38, 97, 124, 5, 97, 87, 85, 106, 12, 31, 87, 6, 77
 };
 
 int32_t matrixBGlobal[N * N] = {
-        69, 96, 71, 89, 127, 108, 96, 121, 64, 65, 62, 91, 73, 0, 0, 0,
-        9, 67, 113, 48, 47, 53, 96, 66, 7, 63, 17, 9, 8, 0, 0, 0,
-        107, 45, 112, 33, 114, 48, 102, 70, 52, 47, 34, 81, 17, 0, 0, 0,
-        38, 15, 61, 1, 104, 82, 68, 53, 69, 110, 12, 25, 46, 0, 0, 0,
-        111, 89, 54, 0, 107, 81, 127, 124, 36, 17, 99, 117, 75, 0, 0, 0,
-        125, 72, 48, 67, 31, 104, 64, 98, 94, 57, 81, 15, 16, 0, 0, 0,
-        111, 16, 127, 119, 88, 41, 75, 125, 22, 50, 120, 6, 81, 0, 0, 0,
-        75, 7, 78, 38, 35, 115, 114, 37, 66, 106, 64, 91, 97, 0, 0, 0,
-        75, 102, 84, 112, 65, 76, 87, 22, 45, 100, 19, 18, 89, 0, 0, 0,
-        27, 25, 109, 18, 116, 19, 116, 33, 103, 31, 29, 78, 8, 0, 0, 0,
-        24, 12, 86, 20, 32, 53, 31, 13, 51, 36, 100, 56, 44, 0, 0, 0,
-        13, 8, 54, 24, 101, 73, 115, 120, 56, 23, 63, 39, 93, 0, 0, 0,
-        77, 50, 108, 56, 106, 58, 121, 74, 70, 88, 19, 49, 83, 0, 0, 0
+        69, 96, 71, 89, 127, 108, 96, 121, 64, 65, 62, 91, 73,
+        9, 67, 113, 48, 47, 53, 96, 66, 7, 63, 17, 9, 8,
+        107, 45, 112, 33, 114, 48, 102, 70, 52, 47, 34, 81, 17,
+        38, 15, 61, 1, 104, 82, 68, 53, 69, 110, 12, 25, 46,
+        111, 89, 54, 0, 107, 81, 127, 124, 36, 17, 99, 117, 75,
+        125, 72, 48, 67, 31, 104, 64, 98, 94, 57, 81, 15, 16,
+        111, 16, 127, 119, 88, 41, 75, 125, 22, 50, 120, 6, 81,
+        75, 7, 78, 38, 35, 115, 114, 37, 66, 106, 64, 91, 97,
+        75, 102, 84, 112, 65, 76, 87, 22, 45, 100, 19, 18, 89,
+        27, 25, 109, 18, 116, 19, 116, 33, 103, 31, 29, 78, 8,
+        24, 12, 86, 20, 32, 53, 31, 13, 51, 36, 100, 56, 44,
+        13, 8, 54, 24, 101, 73, 115, 120, 56, 23, 63, 39, 93,
+        77, 50, 108, 56, 106, 58, 121, 74, 70, 88, 19, 49, 83
 };
 
-int32_t matrixB_transposed[N * N];
-// Calculates AB + A + B
-//void naiveMatrixComputation() {
-//    int8_t c, d, k;
-//    int32_t sum;
-//    for (c = 0; c < N; c++) {
-//        for (d = 0; d < N; d++) {
-//            sum = 0;
-//            for (k = 0; k < N; ++k) {
-//                sum += matrixAGlobal[c][k] * matrixBGlobal[k][d];
-//            }
-//            sum += matrixAGlobal[c][d] + matrixBGlobal[c][d];
-//            matrixCGlobal[c][d] = sum;
-//        }
-//    }
-//}
+// Threads per CTA dimension
+constexpr int THREADS = 16;
 
-// Calculates AB + A + B
-__global__ void
-gpuMatrixComputation(const int32_t* matrixA, const int32_t* matrixB, int32_t* matrixC) {
-    int8_t c, d, k;
-    for (c = 0; c < N; ++c) {
-        for (d = 0; d < N; ++d) {
-            matrixC[c * N + d] = 0;
-            for (k = 0; k < N; ++k) {
-                matrixC[c * N + d] += matrixA[c * N + k] * matrixB[k * N + d];
-            }
-            matrixC[c * N + d] += matrixA[c * N + d] + matrixB[c * N + d];
+// Padded matrix dimensions
+constexpr int M_padded = M + THREADS - M % THREADS;
+constexpr int N_padded = N + THREADS - N % THREADS;
+constexpr int K_padded = K + THREADS - K % THREADS;
+
+// Size of shared memory per TB
+constexpr int SHMEM_SIZE = THREADS * THREADS;
+
+__global__ void matrixMul(const int* a, const int* b, int* c) {
+    // Compute each thread's global row and column index
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Statically allocated shared memory
+    __shared__ int s_a[SHMEM_SIZE];
+    __shared__ int s_b[SHMEM_SIZE];
+
+    // Accumulate in temporary variable
+    int tmp = 0;
+
+    // Sweep tile across matrix
+    for (int i = 0; i < K_padded; i += blockDim.x) {
+        // Load in elements for this tile
+        s_a[threadIdx.y * blockDim.x + threadIdx.x] = a[row * K + i + threadIdx.x];
+        s_b[threadIdx.y * blockDim.x + threadIdx.x] = b[i * N + threadIdx.y * N + col];
+
+        // The C = A + B part
+        tmp += s_a[threadIdx.y * blockDim.x + threadIdx.x] + s_b[threadIdx.y * blockDim.x + threadIdx.x];
+
+        // Wait for both tiles to be loaded in before doing computation
+        __syncthreads();
+
+        // Do matrix multiplication on the small matrix
+        for (int j = 0; j < blockDim.x; j++) {
+            tmp += s_a[threadIdx.y * blockDim.x + j] * s_b[j * blockDim.x + threadIdx.x];
         }
+        
+        // Wait for all threads to finish using current tiles before loading in new
+        // ones
+        __syncthreads();
     }
+
+    // Write back results
+    if (row < M && col < N) c[row * N + col] = tmp;
 }
 
-// Calculates AB + A + B
-__global__ void
-gpuParallelMatrixComputation(int32_t* matrixA, int32_t* matrixB, int32_t* matrixC,  int size) {
-    int x = blockIdx.y * blockDim.y + threadIdx.y;
-    int y = blockIdx.x * blockDim.x + threadIdx.x;
-    int sum = 0;
-    if (x < 13 && y < 13) {
-#pragma unroll
-        for (int k = 0; k < 12; k += 4) {
-            int4 a_tmp = reinterpret_cast<int4*>(&matrixA[x * N + k])[0];
-            sum += a_tmp.x * matrixB[(k + 0) * size + y];
-            sum += a_tmp.y * matrixB[(k + 1) * size + y];
-            sum += a_tmp.z * matrixB[(k + 2) * size + y];
-            sum += a_tmp.w * matrixB[(k + 3) * size + y];
-            
+// Check result on the CPU
+// MxN = MxK * KxN
+void verify_result(vector<int>& a, vector<int>& b, vector<int>& c) {
+    // For every row...
+    for (int row = 0; row < M_padded; row++) {
+        if (row >= M) continue;
+        // For every column...
+        for (int col = 0; col < N_padded; col++) {
+            if (col >= N) continue;
+            // For every element in the row-column pair
+            int tmp = 0;
+            for (int i = 0; i < K_padded; i++) {
+                // Accumulate the partial results
+                tmp += a[row * K + i] * b[i * N + col];
+            }
+
+            // Check against the CPU result
+            assert(tmp == c[row * N + col]);
         }
-        sum += matrixA[x * size + 12] * matrixB[12 * size + y];
-        sum += matrixA[x * size + y] + matrixB[x * size + y];
-        matrixC[x * size + y] = sum;
     }
 }
 
 int main() {
-    /*
-     * Measure the time it takes to execute 1000 times
-     */
+    // Size (in bytes) of matrix
+    // MxN = MxK * KxN
+    size_t bytes_a = M_padded * K_padded * sizeof(int);
+    size_t bytes_b = K_padded * N_padded * sizeof(int);
+    size_t bytes_c = M * N * sizeof(int);
 
-    std::cout << "Malloc device mem" << std::endl;
-    for (int c = 0; c < N; ++c)
-        for (int d = 0; d < N; ++d)
-            matrixB_transposed[c * N + d] = matrixBGlobal[d * N + c];
+    // Host vectors
+    vector<int> h_a(M_padded * K_padded);
+    vector<int> h_b(K_padded * N_padded);
+    vector<int> h_c(M * N);
 
-
-    int32_t* gpu_a, * gpu_b;
-    int32_t* gpu_c;
-
-    int32_t* c_out;
-    c_out = (int32_t*)malloc(N * N * sizeof(int32_t));
-
-    // We need variables accessible to the GPU,
-    // so cudaMallocManaged provides these
-    if (cudaMallocManaged(&gpu_a, N * N * sizeof(int32_t)) != 0) {
-        std::cout << "malloc failed" << std::endl;
+    // Initialize matrices
+    // Padded matrix A
+    for (int i = 0; i < M_padded; i++) {
+        for (int j = 0; j < K_padded; j++) {
+            if (i < M && j < K) h_a[i * K + j] = matrixAGlobal[i * K + j];
+        }
     }
-    if (cudaMallocManaged(&gpu_b, N * N * sizeof(int32_t)) != 0) {
-        std::cout << "malloc failed" << std::endl;
-    }
-
-    if (cudaMallocManaged(&gpu_c, N * N * sizeof(int32_t)) != 0) {
-        std::cout << "malloc failed" << std::endl;
+    // Padded matrix B
+    for (int i = 0; i < K_padded; i++) {
+        for (int j = 0; j < N_padded; j++) {
+            if (i < K && j < N) h_b[i * N + j] = matrixBGlobal[i * K + j];
+        }
     }
 
-    std::cout << "Move to device mem" << std::endl;
+    // Allocate device memory
+    int* d_a, * d_b, * d_c;
+    cudaMalloc(&d_a, bytes_a);
+    cudaMalloc(&d_b, bytes_b);
+    cudaMalloc(&d_c, bytes_c);
 
-    if (cudaMemcpy(gpu_a, matrixAGlobal, (N * N * sizeof(int32_t)), cudaMemcpyHostToDevice) != 0) {
-        std::cout << "memcpy failed" << std::endl;
-    }
+    // Copy data to the device
+    cudaMemcpy(d_a, h_a.data(), bytes_a, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), bytes_b, cudaMemcpyHostToDevice);
 
-#ifdef TRANSPOSED
-    if (cudaMemcpy(gpu_b, matrixB_transposed, (N * N * sizeof(int32_t)), cudaMemcpyHostToDevice) != 0) {
-        std::cout << "memcpy failed" << std::endl;
-    }
-#else
-    if (cudaMemcpy(gpu_b, matrixBGlobal, (N * N * sizeof(int32_t)), cudaMemcpyHostToDevice) != 0) {
-        std::cout << "memcpy failed" << std::endl;
-    }
-#endif
+    // Blocks per grid dimension (assumes THREADS divides M and N evenly)
+    int BLOCKS_X = N_padded / THREADS;
+    int BLOCKS_Y = M_padded / THREADS;
 
-    std::cout << "Computation" << std::endl;
-
-    int threads = 32;
-    int blocks = (N + threads - 1) / threads;
-    dim3 THREADS(threads, threads);
-    dim3 BLOCKS(blocks, blocks);
+    // Use dim3 structs for block  and grid dimensions
+    dim3 threads(THREADS, THREADS);
+    dim3 blocks(BLOCKS_X, BLOCKS_Y);
 
     unsigned long time_taken = 0;
-    for (uint16_t i = 0; i < 1e4; i++) {
+    for (uint16_t i = 0; i < 1e3; i++) {
         auto cpu_start = Clock::now();
-        //88593
-       // gpuMatrixComputation<<<1, 1>>>(gpu_a, gpu_b, gpu_c);
 
-        gpuParallelMatrixComputation <<<BLOCKS, THREADS >>> (gpu_a, gpu_b, gpu_c, N);
+        // Launch kernel
+        matrixMul << <blocks, threads >> > (d_a, d_b, d_c);
 
-        cudaDeviceSynchronize();
         auto cpu_end = Clock::now();
 
         time_taken += std::chrono::duration_cast<std::chrono::nanoseconds>(cpu_end - cpu_start).count();
     }
-    /*
-     * Print the resultating matrix
-     */
-    int8_t c, d;
+    
 
-    std::cout << "Copy from device to host" << std::endl;
-    int cudaError = cudaMemcpy(c_out, gpu_c, (N * N * sizeof(int32_t)), cudaMemcpyDeviceToHost);
-    if (cudaError != 0) {
-        std::cout << "memcpy output failed: " << cudaError << std::endl;
-    }
+    // Copy back to the host
+    cudaMemcpy(h_c.data(), d_c, bytes_c, cudaMemcpyDeviceToHost);
 
-    std::cout << "Printing mem" << std::endl;
+    // Check result
+    // verify_result(h_a, h_b, h_c);
 
-    for (c = 0; c < N; c++) {
-        for (d = 0; d < N; d++) {
-            std::cout << std::to_string((int32_t)c_out[c * N + d]) + "\t";
+    for (int c = 0; c < N; c++) {
+        for (int d = 0; d < N; d++) {
+            std::cout << std::to_string((int32_t)h_c[c * N + d]) + "\t";
         }
         std::cout << "\n";
     }
-
-    std::cout << "Freeing mem" << std::endl;
-
-    cudaFree(gpu_a);
-    cudaFree(gpu_b);
-    cudaFree(gpu_c);
-
     std::cout << "Calculation took: " << std::to_string(time_taken / 1000) << " nanoseconds" << std::endl;
+    cout << "COMPLETED SUCCESSFULLY\n";
+
+    // Free memory on device
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
 
     return 0;
 }
